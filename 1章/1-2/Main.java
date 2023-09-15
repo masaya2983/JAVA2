bd9d56f8d1ab1fd61badb4b053abbfb4da1e0d56
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-15 12:42:06 +0000

    2週目

--- 1章/1-2/Main.java
+++ 1章/1-2/Main.java
@@ -2,6 +2,6 @@ public class Main{
   public static void main(String[]args){
     int age;
     age = 30;
-    System.out.println(age)
+    System.out.println(age);
   }
 }
---
+++ 1章/1-3/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+  public static void main(String[]args){
+    int age = 20;
+    System.out.println("私の年齢" + age);
+    age = 31;
+    System.out.println("・・・いや本当の年齢は" + age);
+  }
+}
