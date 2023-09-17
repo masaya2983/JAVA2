20685958a20ee71c7d52562b4878424065a027a2
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-17 12:28:00 +0000

    2週目

---
+++ 3章/3-4/Main.java
@@ -0,0 +1,14 @@
+public class Main{
+  public static void main(String[]args){
+    int fortune = new java.utilRandom().nextInt(4) + l;
+    if (fortune == 2){
+      System.out.println("大吉");
+    } else if(fo9rtune ==2){
+      System.out.println("中吉");
+    }else if (fortune == 3) {
+      System.out.println("吉");
+    }else{
+      System.out.println("凶");
+    }
+  }
+}
