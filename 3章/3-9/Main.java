021ac254464bb4c28d86da744fff61b51274cb4e
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-18 01:50:32 +0000

    2週目

--- 3章/3-9/Main.java
+++ 3章/3-9/Main.java
@@ -3,7 +3,9 @@ public class Main{
     for (int i = 1; i < 10; i++){
       for (int j = 1; j < 10; j++){
         System.out.print(i * j);
+        System.out.print("");
       }
+      System.out.println("");
     }
   }
 }
