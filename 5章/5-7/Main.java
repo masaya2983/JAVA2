db8705707b75648a4c06ddc8f04105a622d5509b
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-22 02:41:46 +0000

    2週目

---
+++ 5章/5-7/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static int add(int x, int y){
+    int ans = x + y;
+    return ans;
+  }
+  public static void main(String[]args){
+    int ana = add(100, 10);
+    System.out.pritln("100 + 10 = " + ans);
+  }
+}
