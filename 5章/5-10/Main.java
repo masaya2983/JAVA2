7b133ff5d55cfaa7f82cbd93e33908f0bf04391b
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-23 01:34:40 +0000

    2週目

---
+++ 5章/5-10/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+  //1つ目のaddメゾット
+  public static int add(int x, int y){
+    return x +y;
+  }
+  //2つ目のaddメゾット
+  public static double add(double x, double y){
+    return x + y;
+  }
+  public static void main(String[]args){
+    System.out.println(add(10,20));
+    System.out.println(add(3.5,2.7));
+    System.out.println(add("Hello","World"));
+  }
+}
