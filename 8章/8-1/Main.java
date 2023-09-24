25d7e56d337dd0beb49ad9f1903b5db231896256
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-24 03:06:10 +0000

    2週目

---
+++ 8章/8-1/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+  public static void main(String[] args){
+    //(以下のいないようをJavaで記述していく)
+    //勇者よこの仮想世界に生まれよ
+    //お化けキノコよ、この仮想世界に生まれよ!
+    //勇者よ戦え!
+    //お化けキノコよ、逃げろ!
+  }
+}
