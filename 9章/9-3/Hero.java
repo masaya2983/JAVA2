4fafd6f94640d0b6dcfe49c84ca0708e04541197
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-28 01:26:46 +0000

    2週目

---
+++ 9章/9-3/Hero.java
@@ -0,0 +1,11 @@
+public class Sword{
+  String name;
+  int.hp
+  sword sword;
+  public void attack(){
+    System.out.println(this.name + "は攻撃した！");
+    System.out.println("敵に5ポイントのダメージをあたえた！");
+  }
+}
+  
+  
