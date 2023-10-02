cb94071bc1044b76fba57908ea89ee78eaff046f
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-02 12:57:46 +0000

    2週目

---
+++ 11章/11-2/Chacter.java
@@ -0,0 +1,12 @@
+public class Character{
+  String name;
+  int hp;
+  //逃げる
+  public void run(){
+    System.out.println(this.name + "は逃げ出した！");
+  }
+  //戦う
+  public class void attack(Matango m){
+    
+  }
+}
