e364121ff6d00ddad40640f600a9ebba8629387d
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-04 01:41:06 +0000

    2週目

---
+++ 11章/11-15/KyotCleaningShop.java
@@ -0,0 +1,16 @@
+public class KyotoCleaningShop implements CleaningService{
+  String ownerName;
+  String address;
+  String phone;
+  /*シャツを洗う*/
+  public Shirt washShirt(Shirt s){
+    //大型洗濯機15分
+    return t;
+    
+  }
+/*コートを洗う*/
+public Coat washCoat(Coat c){
+  //ドライ20分
+  return c;
+}
+}
