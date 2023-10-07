eb6553295e8834f76137eddec7f9449342cda5f7
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-07 02:07:13 +0000

    2週目

---
+++ 12章/12-7/Main.java
@@ -0,0 +1,16 @@
+public class Main{
+  public static void main(String[] ){
+    Character [] c = new Character[5];
+    c[0]= new Hero;
+    c[1] new Hero;
+    c[2] new Thief;
+    c[3] new Thief;
+    c[4] new Wizard;
+    c[5] new Wizard;
+    
+    //宿屋に泊まる
+    for (Character ch : c){
+      ch.hp += 50;
+    }
+  }
+}
