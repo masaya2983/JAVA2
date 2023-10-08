b7c3243559d5d539caae7a64dda295c7783e7cff
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-08 12:42:58 +0000

    2週目

---
+++ 12章/12-10/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+  public static void main(String[] ){
+    Monster [] Monsters = new Character[5];
+    monster[0]= new Slime();
+    monster[1] new Goblin();
+    monster[2] new DeathBat;
+    for (Monster m :monster){
+     m.run();
+    }
+  }
+}
