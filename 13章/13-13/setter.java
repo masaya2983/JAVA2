f052c6185f10ded1547c0e9dfc0ddf2697d1a31b
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-13 12:42:02 +0000

    2週目

--- 13章/13-13/setter.java
+++ 13章/13-13/setter.java
@@ -1,6 +1,13 @@
 private String name;
 public void setName (String name) {
   if (name == null){
-    throw new IllgalArgumentException("名前がnullである。処理を中断。")
+    throw new IllgalArgumentException("名前がnullである。処理を中断。");
   }
+  if (name.length() <= 1){
+    throw new IllegalArgumentExceotion("名前が短すぎる。処理を中断。");
+  }
+  if (name.length() >= 8){
+    throw new IllegalArgumentExceotion("名前が長すぎる。処理を中断。");
+  }
+  this.name
 }
