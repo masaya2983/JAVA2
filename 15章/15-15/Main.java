42da231cf21b48f4356c0b0a080ebc67a2886ce6
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-21 14:07:08 +0000

    2週目

---
+++ 15章/15-15/Main.java
@@ -0,0 +1,12 @@
+import java.time.*;
+
+public class Main {
+  public static void main(String[] args) {
+    
+    LocalDateTime l1 = LocalDateTime.now();
+    LocalDateTime l2 = LocalDateTime.of(2020,1,1,9,5,0,0);
+    
+    Zoned DateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
+    LocalDateTime l3 = z1.toLocalDateTime();
+  }
+}
