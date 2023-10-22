d76d9498f8729754e4e7a3f93d9968bbb0fa99da
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-22 12:45:39 +0000

    2週目

---
+++ 16章/16-5/Main.java
@@ -0,0 +1,15 @@
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main{
+  public static void main(String[] args) {
+    Set<String> words = new TreeSet<String>();
+    word.add("dog");
+    word.add("cat");
+    word.add("wolf");
+    word.add("panda");
+    for (String s : words) {
+      System.out.print(S + "→");L
+    }
+  }
+}
