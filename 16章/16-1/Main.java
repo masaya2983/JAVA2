9fe11480593eb73a4ca452e5b905ffaa9a3575ba
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-22 02:39:36 +0000

    2週目

---
+++ 16章/16-1/Main.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Main{
+  public static void main(String[] args){
+    ArrayList<Integer>Points = new ArrayList<Integer>();
+    points.add(10);
+    points.add(80);
+    points.add(75);
+    for (int i : Points){
+      System.out.println(i);
+    }
+  }
+}
