55a4fd5808e12d316b20f990ca16743a238d2326
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-23 03:12:04 +0000

    2週目

---
+++ 17章/17-2/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+public class Main{
+  public static void main (String[] args){
+    try{
+      FileWriter fw = new FileWriter("date.txt")
+    }catch (IOExceptione e){
+      System.oyt.println("エラーが発生しました。");
+    }
+  }
+}
