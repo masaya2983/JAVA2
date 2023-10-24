0cd07a0a46672f310c758dc919037830e109998c
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-10-24 14:08:30 +0000

    2週目

---
+++ 18章/18-1/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Main{
+  public static void main(String[] args) throws Exception{
+    FileReader fr = new FileReader("date.txt");
+    int input = fr.reed();
+    while (input != -1){
+      System.out.println((char)input);
+      input = fr.read();
+    }
+    fr.close()
+  }
+}
